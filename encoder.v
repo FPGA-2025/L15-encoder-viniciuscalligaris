b00e05306c06498685ddf6642c93fcd24fc28c32
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-05-05 20:15:03 -0300

    Finalizado

--- encoder.v
+++ encoder.v
@@ -8,5 +8,43 @@ module Encoder (
     output reg A,
     output reg B
 );
-    
+    reg [1:0] sinal;
+
+    always @(sinal) begin
+        A = sinal[1];
+        B = sinal[0];
+    end
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            sinal <= 2'b00;
+        end else begin
+        if (horario && antihorario) begin
+            sinal <= 2'b00;
+        end else begin
+        if (horario && !antihorario) begin
+            case (sinal)
+                2'b00: sinal <= 2'b10;
+                2'b10: sinal <= 2'b11;
+                2'b11: sinal <= 2'b01;
+                2'b01: sinal <= 2'b00;
+                default: sinal <= 2'b00;
+            endcase
+        end else begin
+        if (antihorario && !horario) begin
+            case (sinal)
+                2'b00: sinal <= 2'b01;
+                2'b01: sinal <= 2'b11;
+                2'b11: sinal <= 2'b10;
+                2'b10: sinal <= 2'b00;
+                default: sinal <= 2'b00;
+            endcase
+        end else begin
+            sinal <= 2'b00;
+        end
+        end
+        end
+        end
+    end
+   
 endmodule
--- read_encoder.v
+++ read_encoder.v
@@ -7,5 +7,21 @@ module Read_Encoder (
 
     output reg [1:0] dir
 );
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            dir <= 2'b00;
+        end else begin
+        if (A) begin
+            dir <= 2'b01; 
+        end else begin
+        if (B) begin
+            dir <= 2'b10; 
+        end else begin
+            dir <= 2'b00;
+        end
+        end
+        end
+    end
     
 endmodule
